0d11f4a5d336880aa838e05f7adc5a1bb42a1925
Quillian <quillian.chardon@synerg-in.com>
2021-09-06 10:51:05 +0200

    découverte des fonctionnalité avancé du compo form

---
+++ src/Form/ProductType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Form\DataTransformer\CentimesTransformer;
+use App\Form\Type\PriceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name', TextType::class)
+            ->add('shortDescription', TextareaType::class)
+            ->add('price', PriceType::class)
+            ->add('mainPicture', UrlType::class, [
+                'label' => 'Image du produit',
+                'attr' => ['placeholder' => 'tapez un url d\'image']
+            ])
+            ->add('category', EntityType::class, [
+                'placeholder' => '-- Choisir une catégorie --',
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ]);
+
+        // $builder->get('price')->addModelTransformer(new CentimesTransformer);
+        // $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() * 100);
+        //     }
+        // });
+
+        // $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+        //     $form = $event->getForm();
+        //     /** @var Product */
+        //     $product = $event->getData();
+
+        //     if ($product->getPrice() !== null) {
+        //         $product->setPrice($product->getPrice() / 100);
+        //     }
+        // });
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
